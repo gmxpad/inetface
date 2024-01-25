a3816cf2f145b7314b7c589a172a297c2a0d2272
scohph <scriptesinc@gmail.com>
2024-01-25 23:12:38 +0300

    skale network added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AccessGames from "@/components/accessGames";
 import FAQ from "@/components/faq";
 import Features from "@/components/features";
@@ -6,8 +6,36 @@ import HeroHeader from "@/components/heroHeader";
 import HowToJoin from "@/components/howToJoin";
 import IgoAndIno from "@/components/igoAndIno";
 import PopulerGames from "@/components/populerGames";
+import { useAccount, useNetwork, useSwitchNetwork } from "wagmi";
 
 export default function Home() {
+  const SKALE_CHAIN_ID: number = 1351057110;
+
+  const {
+    address: userAddress,
+    isConnected: connect,
+    isDisconnected: disconnect,
+  } = useAccount();
+
+  const { chains, error, isLoading, pendingChainId, switchNetwork } =
+    useSwitchNetwork();
+  const { chain } = useNetwork();
+  const selectedObject: any = chains.find((item) => item.id === SKALE_CHAIN_ID);
+
+  const FSCNetworkFunction = () => {
+    if (chain) {
+      if (SKALE_CHAIN_ID > 0 && chain?.id !== SKALE_CHAIN_ID) {
+        switchNetwork?.(selectedObject.id);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (connect === true) {
+      FSCNetworkFunction();
+    }
+  }, [chain, connect, userAddress]);
+
   return (
     <>
       <div className="flex flex-col sm:gap-20 md:gap-48 sm:px-[5%] md:px-[10%]">
