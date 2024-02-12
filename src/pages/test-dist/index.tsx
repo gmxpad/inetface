973f3e50d8eef944f5ae11517153361bade718f5
scohph <scriptesinc@gmail.com>
2024-02-12 19:03:41 +0300

    test page --g -v-test-dist

---
+++ src/pages/test-dist/index.tsx
@@ -0,0 +1,76 @@
+import ErrorModal from "@/components/modals/errorModal";
+import SuccessModal from "@/components/modals/successModal";
+import WaitModal from "@/components/modals/waitModal";
+import { GetContract, fetchDistTokenContract } from "@/scripts/contracts";
+import { getSigner } from "@/scripts/scripts";
+import { Button } from "@nextui-org/react";
+import {
+  useWeb3ModalAccount,
+  useWeb3ModalProvider,
+} from "@web3modal/ethers/react";
+import React, { useState } from "react";
+
+export default function TestDist() {
+  const [waitModalOpen, setWaitModalOpen] = useState<boolean>(false);
+  const [errorModalOpen, setErrorModalOpen] = useState<boolean>(false);
+  const [successModalOpenForClaim, setSuccessModalOpenForClaim] =
+    useState<boolean>(false);
+
+  const handleWaitModalOpen = () => {
+    setWaitModalOpen(true);
+  };
+  const handleWaitModalClose = () => {
+    setWaitModalOpen(false);
+  };
+
+  const handleErrorModalOpen = () => {
+    setErrorModalOpen(true);
+  };
+  const handleErrorModalClose = () => {
+    setErrorModalOpen(false);
+  };
+  const handleSuccessModalOpenForClaim = () => {
+    setSuccessModalOpenForClaim(true);
+  };
+  const { address, chainId, isConnected } = useWeb3ModalAccount();
+  const { walletProvider } = useWeb3ModalProvider();
+
+  const claimBTN = async () => {
+    if (isConnected === true) {
+      try {
+        handleWaitModalOpen();
+        const signer = await getSigner(walletProvider);
+        const contract = GetContract(fetchDistTokenContract.address);
+        const tx = await contract
+          ?.connect(signer)
+          // @ts-ignore
+          .claimTokens();
+        await tx.wait();
+        handleWaitModalClose();
+        handleSuccessModalOpenForClaim();
+      } catch (error) {
+        handleWaitModalClose();
+        handleErrorModalOpen();
+        console.log(error);
+      }
+    }
+  };
+  return (
+    <>
+      <WaitModal isOpen={waitModalOpen} onClose={handleWaitModalClose} />
+      <ErrorModal isOpen={errorModalOpen} onClose={handleErrorModalClose} />
+      <SuccessModal
+        isOpen={successModalOpenForClaim}
+        desc={"You have successfully claimed your test tokens."}
+      />
+      <div className="py-24 flex justify-center items-center">
+        <Button
+          radius="sm"
+          onClick={() => claimBTN()}
+          className="bg-[#a664fe] text-white px-12 z-10">
+          Claim
+        </Button>
+      </div>
+    </>
+  );
+}
--- src/scripts/contracts.ts
+++ src/scripts/contracts.ts
@@ -1,5 +1,7 @@
 import DiamondABI from "./DiamondABI.json";
 import ERC20ABI from "./ERC20ABI.json";
+import Distributor from "./Distributor.json";
+
 import { Contract, JsonRpcProvider, BrowserProvider } from "ethers";
 
 interface contractInterfaces {
@@ -15,18 +17,24 @@ export const SkaleChaosTestnet = {
 
 // FETCH CONTRACTS
 export const fetchDiamondContract: contractInterfaces = {
-  address: "0xF1dA369F44e612dAf62C14141Af2B0b01375fD48",
+  address: "0x478277D03081553b7B0bD0348EEBED69F17F6AF2",
   abi: DiamondABI.abi,
   chainId: SkaleChaosTestnet.id,
 };
 export const fetchGMXTokenContract: contractInterfaces = {
-  address: "0xc7a1568b9e05eb49B48D18D923e26A7936a105E9",
+  address: "0xdD3932ad40716aBa856694a42A23EB66e1A57BF9",
   abi: ERC20ABI.abi,
   chainId: SkaleChaosTestnet.id,
 };
 export const fetchXXTokenContract: contractInterfaces = {
-  address: "0xc60Dd8B0FC103190cA3D85C26B2e78F3aE05De2D",
+  address: "0x2E439f0fc167e00c8c4DB7B774E3D1297895db22",
   abi: ERC20ABI.abi,
+  chainId: SkaleChaosTestnet.id,
+};
+
+export const fetchDistTokenContract: contractInterfaces = {
+  address: "0x4406cE68D17f82fb5dcEAbE617E579F5Ee685206",
+  abi: Distributor.abi,
   chainId: SkaleChaosTestnet.id,
 };
 
@@ -48,6 +56,12 @@ export function GetContract(address: `0x${string}` | undefined | string) {
     return new Contract(
       fetchXXTokenContract.address,
       fetchXXTokenContract.abi,
+      Provider
+    );
+  } else if (address === fetchDistTokenContract.address) {
+    return new Contract(
+      fetchDistTokenContract.address,
+      fetchDistTokenContract.abi,
       Provider
     );
   }
